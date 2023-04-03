4a277cb3daad6a5e8272af13b5b539b34e507c22
Tadeusz <dev@3win.pl>
2023-04-03 22:46:02 +0200

    silentsymphony

--- silentsymphony/flutter_service_worker.js
+++ silentsymphony/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5326f9e90ed8490a7022c8c4a9d0b58a",
-"index.html": "c2307b11b7eb5832476ff4a549609bd1",
-"/": "c2307b11b7eb5832476ff4a549609bd1",
-"main.dart.js": "25beb885e6709d2cd5add9e1ed9a3ca7",
+"index.html": "01df073d5398d9749c7962ba41fd3c0a",
+"/": "01df073d5398d9749c7962ba41fd3c0a",
+"main.dart.js": "bfdc4b74c8db70bac6d731452530edd9",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "5a05baf9c836ed70347a785c7cd92773",
 "assets/AssetManifest.json": "9dd6b809c9581a4c3ed42fb2ef504aaf",
-"assets/NOTICES": "a8e0a323c5463901fc7a25845daa7d90",
+"assets/NOTICES": "eaa6c60f855fe4e6643b830801b68a98",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
