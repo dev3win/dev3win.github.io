462f04f1102f1e59935f2bd526e660a6cd56737f
Tadeusz <dev@3win.pl>
2023-04-14 11:57:17 +0200

    silentsymphony

--- silentsymphony/flutter_service_worker.js
+++ silentsymphony/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5326f9e90ed8490a7022c8c4a9d0b58a",
-"index.html": "20e74f363da5da3938bf946650b34341",
-"/": "20e74f363da5da3938bf946650b34341",
-"main.dart.js": "9a6fffcc0b7021bb95b219cf347d4edc",
+"index.html": "be08950e06b1bc93b459442738b40dea",
+"/": "be08950e06b1bc93b459442738b40dea",
+"main.dart.js": "56326c745d7d88cf9c23b477b098a9df",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
